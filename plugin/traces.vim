markonm/traces.vim

commit 60a069fbe2043bcc25a31c6a9d5087df3949f41d

Fix: remove useless pattern group

plugin/traces.vim
@@ -452,8 +452,6 @@ function! s:add_flags(pattern, cmdl, type) abort
   endif
 
   let option = ''
-  let group_start = '\%('
-  let group_end   = '\m\)'
 
   " magic
   if has_key(a:cmdl, 'cmd') && a:cmdl.cmd.name =~# '\v^sm%[agic]$'
@@ -506,7 +504,7 @@ function! s:add_flags(pattern, cmdl, type) abort
   endif
 
   let range = '\m\%>'. start .'l' . '\%<' . end . 'l'
-  return range . group_start . option . a:pattern . group_end
+  return range . option . a:pattern
 endfunction
 
 function! s:parse_global(cmdl) abort

commit 52e45260d7d44502c0976516c4f5261746f93a53

Use a group to contain a pattern inside of a range
This is a fix for a regression from
60a069fbe2043bcc25a31c6a9d5087df3949f41d

plugin/traces.vim
@@ -523,7 +523,17 @@ function! s:add_flags(pattern, cmdl, type) abort
   endif
 
   let range = '\m\%>'. start .'l' . '\%<' . end . 'l'
-  return range . option . a:pattern
+
+  " group is necessary to contain pattern inside range when using branches (\|)
+  let group_start = '\%('
+  let group_end   = '\m\)'
+  " add backslash to the end of pattern if it ends with odd number of
+  " backslashes, this is required to properly close group
+  if len(matchstr(a:pattern, '\\\+$')) % 2
+    let group_end = '\' . group_end
+  endif
+
+  return range . group_start . option . a:pattern . group_end
 endfunction
 
 function! s:parse_global(cmdl) abort